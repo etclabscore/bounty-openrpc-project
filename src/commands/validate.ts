0849b8a20d00fdb9c909b47bf234456f370d931d
Haben Amare <haben.amare@outlook.com>
2020-05-13 17:53:48 +0300

    Fix ESLint errors

--- src/commands/validate.ts
+++ src/commands/validate.ts
@@ -1,24 +1,22 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
 
 export default class Validate extends Command {
-  static description = 'validate an OpenRPC document'
+  static description = 'validate an OpenRPC document';
 
   static examples = [
     `$ openrpc-cli validate openrpc.json
 `,
-  ]
+  ];
 
   static flags = {
-    help: flags.help({char: 'h'}),
-  }
+    help: flags.help({ char: 'h' }),
+  };
 
-  static args = [
-    {name: 'file', required: true}
-  ]
+  static args = [{ name: 'file', required: true }];
 
   async run() {
-    const {args, flags} = this.parse(Validate)
+    const { args, flags } = this.parse(Validate);
 
-    this.log(`Validating ${args.file}`)
+    this.log(`Validating ${args.file}`);
   }
 }
