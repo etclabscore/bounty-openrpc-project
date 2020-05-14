f1a1a365d595f85f100741f5fcd00a4c3f1bfc6f
Haben Amare <haben.amare@outlook.com>
2020-05-14 13:58:05 +0300

    feat: add OpenRPC validator

---
+++ src/openrpc-validator.ts
@@ -0,0 +1,32 @@
+import * as Ajv from 'ajv';
+import * as openRpcSchema from './openrpc-meta-schema/1.11.0/open-rpc-meta-schema.json';
+
+const ajv = new Ajv({
+  allErrors: true,
+  logger: false,
+
+  validateSchema: false,
+  missingRefs: 'ignore',
+});
+
+const ajvValidate = ajv.compile(openRpcSchema);
+
+const openrpcValidate = (
+  data: object
+): { valid: boolean; errors: Ajv.ErrorObject[]; hasErrors: boolean } => {
+  const valid = ajvValidate(data);
+
+  let errors: Ajv.ErrorObject[] = [];
+  if (ajvValidate.errors !== null && ajvValidate.errors !== undefined) {
+    errors = ajvValidate.errors;
+  }
+
+  return {
+    valid: valid ? true : false,
+
+    hasErrors: errors.length !== 0,
+    errors,
+  };
+};
+
+export { openrpcValidate };
