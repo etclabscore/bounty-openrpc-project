626d2e00fdd59433c8cb5562bfff8ecb66975fb9
Haben Amare <haben.amare@outlook.com>
2020-05-20 23:54:24 +0300

    docs: add description for the 'inspect' command

--- src/commands/inspect.ts
+++ src/commands/inspect.ts
@@ -15,7 +15,7 @@ import { log, styledString } from '../logger';
 import { openrpcParse } from '../openrpc-parser';
 
 export default class Inspect extends Command {
-  static description = '';
+  static description = 'make requests to methods inside an OpenRPC document';
 
   static examples = [
     `$ openrpc-cli inspect openrpc.json
